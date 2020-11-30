cdc8deb93ea5c37886d79588b7226ae65e251c19
Matt Madison <matt@madison.systems>
2020-11-30 07:36:03 -0800

    Hard-code BCT page/block sizes on t18x/t19x
    The page and block size fields are in the encrypted
    section of the BCT on t18x/t19x platforms, so we can't
    use them for determining how to update the BCT.
    The two sizes vary depending on the boot device (eMMC
    or SPI flash), but otherwise don't change, so just
    use the known values.
    
    Signed-off-by: Matt Madison <matt@madison.systems>

--- bct.h
+++ bct.h
@@ -2,8 +2,8 @@
 #define bct_h__
 /* Copyright (c) 2020 Matthew Madison */
 
-int bct_update_valid_t18x(void *cur_bct, void *cand_bct, unsigned int *block_size, unsigned int *page_size);
-int bct_update_valid_t19x(void *cur_bct, void *cand_bct, unsigned int *block_size, unsigned int *page_size);
+int bct_update_valid_t18x(void *cur_bct, void *cand_bct);
+int bct_update_valid_t19x(void *cur_bct, void *cand_bct);
 int bct_update_valid_t21x(void *cur_bct, void *cand_bct, unsigned int *block_size, unsigned int *page_size);
 
 #endif /* bct_h__ */

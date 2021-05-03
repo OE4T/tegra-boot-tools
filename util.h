86b0022c897b774b4d3b4a0aae21291546a2dc68
Matt Madison <matt@madison.systems>
2021-05-03 05:16:33 -0700

    util.{c,h}: add module
    populated with the set_bootdev_writeable_status function
    that was copy-pasted across multiple other source files.
    
    Signed-off-by: Matt Madison <matt@madison.systems>

---
+++ util.h
@@ -0,0 +1,8 @@
+#ifndef util_h__
+#define util_h__
+/* Copyright (c) 2021, Matthew Madison */
+
+#include <stdbool.h>
+bool set_bootdev_writeable_status(const char *bootdev, bool make_writeble);
+
+#endif /* util_h__ */
